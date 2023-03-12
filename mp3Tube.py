26b51f5c51236a5c7722fc0502a7a20dd51c8eba
mvictor10 <mvictordossantos.limite@gmail.com>
2023-03-12 11:47:09 -0300

    features: add new funcionality

--- mp3Tube.py
+++ mp3Tube.py
@@ -1,9 +1,5 @@
-
-#!/usr/bin/python
-
 from pytube import YouTube
 from moviepy.editor import *
-from sys import argv
 from os import system
 
 #classe para baixar música.
@@ -21,15 +17,6 @@ class MP3Download:
 		clip.write_audiofile(audio.default_filename.replace('.webm', '.mp3'))
 		os.remove(audio.default_filename)
 
-
-if __name__ == "__main__":
-	try:
-		url = argv[1]
-		instance = MP3Download(url)
-		print("Downloading file...")
-		instance.init()
-	except ValueError:
-		print('./mp3Tube.py <url_youtube_video>')
 
 
 
