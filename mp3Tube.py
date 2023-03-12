dbbd05a86a6c6637f0a6926845b9bad25a81686e
mvictor10 <mvictordossantos.limite@gmail.com>
2023-03-12 08:18:55 -0300

    feature: add new funcionality

---
+++ mp3Tube.py
@@ -0,0 +1,32 @@
+
+#!/usr/bin/python
+
+from pytybe import YouTube
+from moviepy.editor import *
+from sys import argv
+
+class MP3Download:
+
+	def __init__ (self, url):
+		self.url = url
+
+	def init(self):
+		video = YouTube(self.url)
+		audio = video.streams.filter(only_audio=True).order_by('abr').last()
+		audio.download()
+		clip = AudioFileClip(audio.default_filename)
+		clip.write_audiofile(audio.default_filename.replace('.webm', '.mp3'))
+		os.remove(audio.default_filename)
+
+
+if __name__ == "__main__":
+	try:
+		url = argv[1]
+		instance = MP3Download(url)
+		print("Downloading file...")
+		instance.init()
+	except ValueError:
+		print('./mp3Tube.py <url_youtube_video>')
+
+
+
